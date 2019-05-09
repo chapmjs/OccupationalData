f7fba89d274173273195ac27a3042a8f0e7b5cd3
John Chapman <684704+chapmjs@users.noreply.github.com>
2019-05-09 20:03:58 +0000

    as.numeric fields for: emp2014, emp2024, numInc, preInc, selfEmp, growRep, medWage

--- app.R
+++ app.R
@@ -12,26 +12,44 @@
 library(shiny)
 library(DT)
 library(dplyr)
+library(readr)
 # library(readxl)
 
+
+## DATA IMPORT
 
 # occ_data <- read_excel("~/R/OccupationalData/Occupational-Data.xlsx")
 # occ_data <- read_excel("Occupational-Data.xlsx")
 occ_data <- read.csv("Occupational-Data.csv", header = TRUE, stringsAsFactors = TRUE)
 
+
+## DATA CLEAN
+
 # remove extra column (this import method results in extra column)
 occ_data <- occ_data[-c(14)]
 
 # rename column names
-names.col <- c("occupation","occ_code","occ_type","2014employment","2024employment","numIncrease","perInc","selfEmpl","growRep","medWage","entryEd","exper","ojt")
+names.col <- c("occupation","occ_code","occ_type","emp2014","emp2024","numInc","perInc","selfEmp","growRep","medWage","entryEdu","exper","ojt")
 names(occ_data) <- names.col
 
-# remove extra rows - only include line item rows
+# remove "Summary" rows - only include "Line item" occ_type rows
 occ_data_li <- occ_data %>% filter(occ_type == "Line item")
 
-# get factors of EntryEd for filter
+# change from string to numeric fields - emp2014, emp2024, numInc, perInc, selfEmp, growRep, medWage
+numericCols <- c("emp2014", "emp2024", "numInc", "perInc", "selfEmp", "growRep", "medWage")
+
+#remove non-numeric characters from medWage:
+occ_data_li$medWage <- parse_number(as.character(occ_data_li$medWage))
+
+# occ_data_li$emp2014 <- as.numeric(occ_data_li$emp2014)
+occ_data_li[,numericCols] <- lapply(occ_data_li[,numericCols], as.numeric)
+
+
+## PREP for App
+
+# get factors of EntryEdu for filter
 # levels(factor(occ_data_li$entryEd))
-entryEdList <- levels(factor(occ_data$entryEd,ordered = TRUE, levels = c(
+entryEduList <- levels(factor(occ_data$entryEdu,ordered = TRUE, levels = c(
                             "No formal educational credential", 
                             "High school diploma or equivalent", 
                             "Some college, no degree", 
@@ -51,9 +69,9 @@ ui <- fluidPage(
     # Sidebar with a slider input to choose an order by field 
     sidebarLayout(
         sidebarPanel(
-            selectInput("entryEdFilter",
+            selectInput("entryEduFilter",
                         "Select Entry Education:",
-                        choices = c("",entryEdList),
+                        choices = c("",entryEduList),
                         selected = ""),
             width = 3
         ),
@@ -69,11 +87,11 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$occupationalData = DT::renderDataTable({
-        if (input$entryEdFilter == "") {
+        if (input$entryEduFilter == "") {
             occ_data_li %>% arrange(occ_code) 
         }
         else {
-        occ_data_li %>% filter(entryEd == input$entryEdFilter) %>% arrange(occ_code)
+        occ_data_li %>% filter(entryEdu == input$entryEduFilter) %>% arrange(occ_code)
         }
     })
 }
